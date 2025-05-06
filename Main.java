7b3317ae1e7079aa22a6725fe2d45cbbabfb2787
Itonda <anthellic@gmail.com>
2025-05-05 21:22:50 -0600

    Fixed a comment

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     // Main method to run the application
     public static void main(String[] args) {
-        // Create an instance of StudentListInput
+        // Create an instance of StudentListMenu
         StudentListMenu inputHandler = new StudentListMenu();
         inputHandler.start(); // Start the input handler to get student data
     }
